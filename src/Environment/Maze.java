6d0eab86dbd3640de6a9d1f09198cecc60c792d1
Filipe Barbosa <filipe.g.barbosa@gmail.com>
2020-11-13 19:46:06 +0000

    Add hunter tile support for move functions

--- src/Environment/Maze.java
+++ src/Environment/Maze.java
@@ -50,21 +50,21 @@ public class Maze {
 		public int getYCoordinate() {
 			return yCoordinate;
 		}
-		
+
 		/**
 		 * 
 		 * @param caught
-		 * @return			If it was set (only works on beasts)
+		 * @return If it was set (only works on beasts)
 		 */
 		public boolean setCaught(boolean caught) {
-			if(isHunter)
+			if (isHunter)
 				return false;
-			
+
 			this.caught = caught;
-			
+
 			return true;
 		}
-		
+
 		public boolean isCaught() {
 			return !isHunter && caught;
 		}
@@ -151,7 +151,7 @@ public class Maze {
 				numTiles.put(this.maze[y][x], currValue + 1);
 			}
 		}
-		
+
 		startingTime = null;
 		endingTime = null;
 	}
@@ -222,29 +222,38 @@ public class Maze {
 
 	/**
 	 * Returns the list of possible movements in a specified tile.
-	 * @param xCoordinate	X coordinate of the current tile;
-	 * @param yCoordinate	Y coordinate of the current tile;
-	 * @return				List of valid moves to make from the current tile.
+	 * 
+	 * @param xCoordinate X coordinate of the current tile;
+	 * @param yCoordinate Y coordinate of the current tile;
+	 * @return List of valid moves to make from the current tile.
 	 */
 	public List<Movement> possibleMovements(int xCoordinate, int yCoordinate) {
-		if(maze[yCoordinate][xCoordinate].equals(Tile.W))
+		if (maze[yCoordinate][xCoordinate].equals(Tile.W))
 			return Collections.emptyList();
 
 		ArrayList<Movement> validMoves = new ArrayList<Movement>();
 
-		if (yCoordinate > 0 && maze[yCoordinate - 1][xCoordinate].equals(Tile.C)) {
+		if (yCoordinate > 0 && (maze[yCoordinate - 1][xCoordinate].equals(Tile.C)
+				|| maze[yCoordinate - 1][xCoordinate].equals(Tile.H)
+						&& maze[yCoordinate][xCoordinate].equals(Tile.H))) {
 			validMoves.add(Movement.Up);
 		}
 
-		if (yCoordinate < maze.length - 1 && maze[yCoordinate + 1][xCoordinate].equals(Tile.C)) {
+		if (yCoordinate < maze.length - 1 && (maze[yCoordinate + 1][xCoordinate].equals(Tile.C)
+				|| maze[yCoordinate + 1][xCoordinate].equals(Tile.H)
+						&& maze[yCoordinate][xCoordinate].equals(Tile.C))) {
 			validMoves.add(Movement.Down);
 		}
 
-		if (xCoordinate > 0 && maze[yCoordinate][xCoordinate - 1].equals(Tile.C)) {
+		if (xCoordinate > 0 && (maze[yCoordinate][xCoordinate - 1].equals(Tile.C)
+				|| maze[yCoordinate][xCoordinate - 1].equals(Tile.H)
+						&& maze[yCoordinate][xCoordinate].equals(Tile.H))) {
 			validMoves.add(Movement.Left);
 		}
 
-		if (xCoordinate < maze[0].length && maze[yCoordinate][xCoordinate + 1].equals(Tile.C)) {
+		if (xCoordinate < maze[0].length && (maze[yCoordinate][xCoordinate + 1].equals(Tile.C)
+				|| maze[yCoordinate][xCoordinate + 1].equals(Tile.H)
+						&& maze[yCoordinate][xCoordinate].equals(Tile.H))) {
 			validMoves.add(Movement.Right);
 		}
 
@@ -268,41 +277,47 @@ public class Maze {
 			return false;
 
 		if (movement.equals(Movement.Up)) {
-			if (entity.yCoordinate > 0 && maze[entity.yCoordinate - 1][entity.xCoordinate].equals(Tile.C)) {
+			if (entity.yCoordinate > 0 && (maze[entity.yCoordinate - 1][entity.xCoordinate].equals(Tile.C)
+					|| maze[entity.yCoordinate - 1][entity.xCoordinate].equals(Tile.H)
+							&& maze[entity.yCoordinate][entity.xCoordinate].equals(Tile.H))) {
 				entity.yCoordinate -= 1;
 				entity.direction = Direction.North;
 			} else
 				return false;
 		} else if (movement.equals(Movement.Down)) {
-			if (entity.yCoordinate < maze.length - 1
-					&& maze[entity.yCoordinate + 1][entity.xCoordinate].equals(Tile.C)) {
+			if (entity.yCoordinate < maze.length - 1 && (maze[entity.yCoordinate + 1][entity.xCoordinate].equals(Tile.C)
+					|| maze[entity.yCoordinate + 1][entity.xCoordinate].equals(Tile.H)
+							&& maze[entity.yCoordinate][entity.xCoordinate].equals(Tile.C))) {
 				entity.yCoordinate += 1;
 				entity.direction = Direction.South;
 			} else
 				return false;
 		} else if (movement.equals(Movement.Left)) {
-			if (entity.xCoordinate > 0 && maze[entity.yCoordinate][entity.xCoordinate - 1].equals(Tile.C)) {
+			if (entity.xCoordinate > 0 && (maze[entity.yCoordinate][entity.xCoordinate - 1].equals(Tile.C)
+					|| maze[entity.yCoordinate][entity.xCoordinate - 1].equals(Tile.H)
+							&& maze[entity.yCoordinate][entity.xCoordinate].equals(Tile.H))) {
 				entity.xCoordinate -= 1;
 				entity.direction = Direction.West;
 			} else
 				return false;
 		} else if (movement.equals(Movement.Right)) {
-			if (entity.xCoordinate < maze[0].length
-					&& maze[entity.yCoordinate][entity.xCoordinate + 1].equals(Tile.C)) {
+			if (entity.xCoordinate < maze[0].length && (maze[entity.yCoordinate][entity.xCoordinate + 1].equals(Tile.C)
+					|| maze[entity.yCoordinate][entity.xCoordinate + 1].equals(Tile.H)
+							&& maze[entity.yCoordinate][entity.xCoordinate].equals(Tile.H))) {
 				entity.xCoordinate += 1;
 				entity.direction = Direction.East;
 			} else
 				return false;
 		} else
 			return false;
-		
+
 		// Check if with this move, a beast was caught
 		wasBeastCaught(entity.xCoordinate, entity.yCoordinate);
 
 		// When the first move is made, the clock starts
-		if(startingTime == null)
+		if (startingTime == null)
 			startingTime = LocalDateTime.now();
-		
+
 		return true;
 	}
 
@@ -376,61 +391,63 @@ public class Maze {
 
 		return entityFound;
 	}
-	
+
 	/**
 	 * Check if all beasts have been caught.
+	 * 
 	 * @return
 	 */
 	public boolean isOver() {
-		for(String entityName : entities.keySet()) {
+		for (String entityName : entities.keySet()) {
 			MazeEntity entity = entities.get(entityName);
-			
-			if(entity != null && !entity.isHunter && !entity.isCaught())
+
+			if (entity != null && !entity.isHunter && !entity.isCaught())
 				return false;
 		}
-		
+
 		// Clock stops when all beasts have been caught
-		if(endingTime == null)
+		if (endingTime == null)
 			endingTime = LocalDateTime.now();
-		
+
 		return true;
 	}
-	
+
 	public String getClock() {
-		if(startingTime == null)
+		if (startingTime == null)
 			return "00:00";
-		
+
 		LocalDateTime endTime = endingTime == null ? LocalDateTime.now() : endingTime;
-		
+
 		Duration duration = Duration.between(startingTime, endTime);
-		
+
 		return String.format("%02d", duration.toMinutes()) + ":" + String.format("%02d", duration.toSecondsPart());
 	}
 
 	/**
 	 * Check if in the specified coordinates there is a catched beast.
+	 * 
 	 * @param xCoordinate
 	 * @param yCoordinate
 	 */
 	private void wasBeastCaught(int xCoordinate, int yCoordinate) {
 		ArrayList<MazeEntity> beastsInCoordinates = new ArrayList<MazeEntity>();
 		boolean hunterInCoordinates = false;
-		
-		for(String entityName : entities.keySet()) {
+
+		for (String entityName : entities.keySet()) {
 			MazeEntity entity = entities.get(entityName);
-			
-			if(entity != null && entity.xCoordinate == xCoordinate && entity.yCoordinate == yCoordinate) {
-				if(entity.isHunter)
+
+			if (entity != null && entity.xCoordinate == xCoordinate && entity.yCoordinate == yCoordinate) {
+				if (entity.isHunter)
 					hunterInCoordinates = true;
 				else {
 					beastsInCoordinates.add(entity);
 				}
 			}
 		}
-		
-		if(hunterInCoordinates) {
+
+		if (hunterInCoordinates) {
 			// Sets each beast in the coordinates as having been caught
-			for(MazeEntity beast : beastsInCoordinates) {
+			for (MazeEntity beast : beastsInCoordinates) {
 				beast.setCaught(true);
 			}
 		}
@@ -484,7 +501,7 @@ public class Maze {
 	public ConcurrentHashMap<String, MazeEntity> getEntities() {
 		return this.entities;
 	}
-	
+
 	public Graph getGraph() {
 		return mazeGraph;
 	}
