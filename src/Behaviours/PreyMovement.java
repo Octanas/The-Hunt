3289019e3057610cf73963f1a69338a4ff377356
Filipe Barbosa <filipe.g.barbosa@gmail.com>
2020-11-13 14:12:18 +0000

    Add ability to stop behaviours when prey is caught

--- src/Behaviours/PreyMovement.java
+++ src/Behaviours/PreyMovement.java
@@ -34,64 +34,86 @@ public class PreyMovement extends TickerBehaviour {
 	}
 
 	@Override
-	protected void onTick() {
+	protected void onTick() {	
 		Maze maze = agent.getMaze();
 
 		if (maze != null) {
 			Random rand = new Random();
 
-			Maze.Direction direction = maze.getEntities().get(agent.getName()).getDirection();
-			Maze.MazeEntity visibleEntity = maze.getVisibleEntity(agent.getName());
+			Maze.MazeEntity self = maze.getEntities().get(agent.getName());
 
-			// If there is a visible hunter, mark the movement in the direction it was seen to be avoided for 5 ticks
-			if (visibleEntity != null && visibleEntity.isHunter) {
-				if (direction.equals(Maze.Direction.North)) {
-					movementsToAvoid.put(Maze.Movement.Up, 5);
-				} else if (direction.equals(Maze.Direction.South)) {
-					movementsToAvoid.put(Maze.Movement.Down, 5);
-				} else if (direction.equals(Maze.Direction.East)) {
-					movementsToAvoid.put(Maze.Movement.Right, 5);
-				} else if (direction.equals(Maze.Direction.West)) {
-					movementsToAvoid.put(Maze.Movement.Left, 5);
-				}
-			}
+			if (!self.isCaught()) {
+				Maze.Direction direction = self.getDirection();
+				Maze.MazeEntity visibleEntity = maze.getVisibleEntity(agent.getName());
 
-			// Get list with all the possible movements
-			ArrayList<Maze.Movement> possibleMovements = new ArrayList<Maze.Movement>(
-					Arrays.asList(Maze.Movement.values()));
-			
-			// Order the movements from best to worst, according to the number of ticks that it should be avoided
-			//
-			// Movements with the same number of ticks that they should be avoided are randomised in the order between them
-			// A movement to go backwards will always be worse than another movement with the same number of ticks that it should be avoided
-			for (int i = 0; i < possibleMovements.size() - 1; i++) {
-				for (int j = 0; j < possibleMovements.size() - i - 1; j++) {
-					if (movementsToAvoid.get(possibleMovements.get(j)) > movementsToAvoid.get(possibleMovements.get(j + 1))
-							|| movementsToAvoid.get(possibleMovements.get(j)) == movementsToAvoid.get(possibleMovements.get(j + 1))
-									&& (direction.equals(Maze.Direction.North) && possibleMovements.get(j).equals(Maze.Movement.Down)
-											|| direction.equals(Maze.Direction.South) && possibleMovements.get(j).equals(Maze.Movement.Up)
-											|| direction.equals(Maze.Direction.East) && possibleMovements.get(j).equals(Maze.Movement.Left)
-											|| direction.equals(Maze.Direction.West) && possibleMovements.get(j).equals(Maze.Movement.Right)
-											|| rand.nextBoolean()) 
-										&& !(direction.equals(Maze.Direction.North) && possibleMovements.get(j + 1).equals(Maze.Movement.Down)
-													|| direction.equals(Maze.Direction.South) && possibleMovements.get(j + 1).equals(Maze.Movement.Up)
-													|| direction.equals(Maze.Direction.East) && possibleMovements.get(j + 1).equals(Maze.Movement.Left)
-													|| direction.equals(Maze.Direction.West) && possibleMovements.get(j + 1).equals(Maze.Movement.Right))) {
-						Maze.Movement movement = possibleMovements.remove(j + 1);
-						possibleMovements.add(j, movement);
+				// If there is a visible hunter, mark the movement in the direction it was seen
+				// to be avoided for 5 ticks
+				if (visibleEntity != null && visibleEntity.isHunter) {
+					if (direction.equals(Maze.Direction.North)) {
+						movementsToAvoid.put(Maze.Movement.Up, 5);
+					} else if (direction.equals(Maze.Direction.South)) {
+						movementsToAvoid.put(Maze.Movement.Down, 5);
+					} else if (direction.equals(Maze.Direction.East)) {
+						movementsToAvoid.put(Maze.Movement.Right, 5);
+					} else if (direction.equals(Maze.Direction.West)) {
+						movementsToAvoid.put(Maze.Movement.Left, 5);
 					}
 				}
+
+				// Get list with all the possible movements
+				ArrayList<Maze.Movement> possibleMovements = new ArrayList<Maze.Movement>(
+						Arrays.asList(Maze.Movement.values()));
+
+				// Order the movements from best to worst, according to the number of ticks that
+				// it should be avoided
+				//
+				// Movements with the same number of ticks that they should be avoided are
+				// randomised in the order between them
+				// A movement to go backwards will always be worse than another movement with
+				// the same number of ticks that it should be avoided
+				for (int i = 0; i < possibleMovements.size() - 1; i++) {
+					for (int j = 0; j < possibleMovements.size() - i - 1; j++) {
+						if (movementsToAvoid.get(possibleMovements.get(j)) > movementsToAvoid
+								.get(possibleMovements.get(j + 1))
+								|| movementsToAvoid.get(possibleMovements.get(j)) == movementsToAvoid
+										.get(possibleMovements.get(j + 1))
+										&& (direction.equals(Maze.Direction.North)
+												&& possibleMovements.get(j).equals(Maze.Movement.Down)
+												|| direction.equals(Maze.Direction.South)
+														&& possibleMovements.get(j).equals(Maze.Movement.Up)
+												|| direction.equals(Maze.Direction.East)
+														&& possibleMovements.get(j).equals(Maze.Movement.Left)
+												|| direction.equals(Maze.Direction.West)
+														&& possibleMovements.get(j).equals(Maze.Movement.Right)
+												|| rand.nextBoolean())
+										&& !(direction.equals(Maze.Direction.North)
+												&& possibleMovements.get(j + 1).equals(Maze.Movement.Down)
+												|| direction.equals(Maze.Direction.South)
+														&& possibleMovements.get(j + 1).equals(Maze.Movement.Up)
+												|| direction.equals(Maze.Direction.East)
+														&& possibleMovements.get(j + 1).equals(Maze.Movement.Left)
+												|| direction.equals(Maze.Direction.West)
+														&& possibleMovements.get(j + 1).equals(Maze.Movement.Right))) {
+							Maze.Movement movement = possibleMovements.remove(j + 1);
+							possibleMovements.add(j, movement);
+						}
+					}
+				}
+
+				int i = 0;
+
+				// Follow the list until a movement is valid (the worst movement will always be
+				// the last one to be tried)
+				while (!maze.moveEntity(agent.getName(), possibleMovements.get(i)) && i != possibleMovements.size())
+					i++;
+
+				// Decrease a tick from each movement (the ones at 0 stay at 0)
+				for (Maze.Movement movement : movementsToAvoid.keySet()) {
+					movementsToAvoid.put(movement, Math.max(0, movementsToAvoid.get(movement) - 1));
+				}
 			}
-
-			int i = 0;
-
-			// Follow the list until a movement is valid (the worst movement will always be the last one to be tried)
-			while (!maze.moveEntity(agent.getName(), possibleMovements.get(i)) && i != possibleMovements.size())
-				i++;
-
-			// Decrease a tick from each movement (the ones at 0 stay at 0)
-			for (Maze.Movement movement : movementsToAvoid.keySet()) {
-				movementsToAvoid.put(movement, Math.max(0, movementsToAvoid.get(movement) - 1));
+			else {
+				stop();	// Ticker Behaviour will stop repeating
 			}
 		}
 	}
--- src/Behaviours/RandomMovement.java
+++ src/Behaviours/RandomMovement.java
@@ -23,22 +23,29 @@ public class RandomMovement extends TickerBehaviour {
 		if (maze != null) {
 			Random rand = new Random();
 
-			Maze.Direction direction = maze.getEntities().get(agent.getName()).getDirection();
+			Maze.MazeEntity self = maze.getEntities().get(agent.getName());
+			
+			if(!self.isCaught()) {
+				Maze.Direction direction = self.getDirection();
 
-			boolean success = false;
+				boolean success = false;
 
-			while (!success) {
-				Maze.Movement chosenMovement;
+				while (!success) {
+					Maze.Movement chosenMovement;
 
-				// This do while makes sure the chosen movement is not a backwards movement
-				do {
-					chosenMovement = Maze.Movement.values()[rand.nextInt(Maze.Movement.values().length)];
-				} while (direction.equals(Maze.Direction.North) && chosenMovement.equals(Maze.Movement.Down)
-						|| direction.equals(Maze.Direction.South) && chosenMovement.equals(Maze.Movement.Up)
-						|| direction.equals(Maze.Direction.East) && chosenMovement.equals(Maze.Movement.Left)
-						|| direction.equals(Maze.Direction.West) && chosenMovement.equals(Maze.Movement.Right));
+					// This do while makes sure the chosen movement is not a backwards movement
+					do {
+						chosenMovement = Maze.Movement.values()[rand.nextInt(Maze.Movement.values().length)];
+					} while (direction.equals(Maze.Direction.North) && chosenMovement.equals(Maze.Movement.Down)
+							|| direction.equals(Maze.Direction.South) && chosenMovement.equals(Maze.Movement.Up)
+							|| direction.equals(Maze.Direction.East) && chosenMovement.equals(Maze.Movement.Left)
+							|| direction.equals(Maze.Direction.West) && chosenMovement.equals(Maze.Movement.Right));
 
-				success = maze.moveEntity(agent.getName(), chosenMovement);
+					success = maze.moveEntity(agent.getName(), chosenMovement);
+				}
+			}
+			else {
+				stop();	// Ticker Behaviour will stop repeating
 			}
 		}
 	}
