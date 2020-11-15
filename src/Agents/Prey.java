577a7c319a44a9b3fdc30c103e27c795958ec9f6
Filipe Barbosa <filipe.g.barbosa@gmail.com>
2020-11-15 12:53:57 +0000

    Add sight behaviour for prey

--- src/Agents/Prey.java
+++ src/Agents/Prey.java
@@ -1,7 +1,10 @@
 package Agents;
 
+import java.util.HashMap;
+
 import Behaviours.PreyMovement;
 import Behaviours.RandomMovement;
+import Behaviours.SightBehaviour;
 import Environment.Maze;
 import jade.core.*;
 import jade.domain.DFService;
@@ -12,13 +15,24 @@ import jade.domain.FIPAAgentManagement.ServiceDescription;
 public class Prey extends Agent {
 
 	private static final long serialVersionUID = 2160457764102179509L;
-	
+
 	private Maze maze;
+
+	/**
+	 * Keeps track of movements the prey should avoid to not run into hunters (for a
+	 * short period of time).
+	 * 
+	 * The Integer represents the number of ticks that a movement should be avoided.
+	 * It decreases for each tick.
+	 * 
+	 * This is only used with the PreyMovement behaviour
+	 */
+	private HashMap<Maze.Movement, Integer> movementsToAvoid;
+
+	private int ticksToAvoid = 3;
 
 	protected void setup() {
 
-		// Registration with the DF
-		// Registration with the DF
 		// Registration with the DF
 		DFAgentDescription agentDescription = new DFAgentDescription();
 		ServiceDescription serviceDescription = new ServiceDescription();
@@ -41,7 +55,12 @@ public class Prey extends Agent {
 			e.printStackTrace();
 		}
 
-		// Adding initial behaviours
+		movementsToAvoid = new HashMap<Maze.Movement, Integer>();
+
+		// Add sight behaviour
+		addBehaviour(new SightBehaviour(this, 100, false));
+
+		// Add movement behavior
 		addBehaviour(new PreyMovement(this, 500));
 		// addBehaviour(new RandomMovement(this, 500));
 	}
@@ -50,4 +69,11 @@ public class Prey extends Agent {
 		return maze;
 	}
 
+	public HashMap<Maze.Movement, Integer> getMovementsToAvoid() {
+		return movementsToAvoid;
+	}
+
+	public int getTicksToAvoid() {
+		return ticksToAvoid;
+	}
 }
--- src/Behaviours/SightBehaviour.java
+++ src/Behaviours/SightBehaviour.java
@@ -1,5 +1,6 @@
 package Behaviours;
 
+import Agents.Prey;
 import Agents.SuperAgent;
 import Environment.Maze;
 import jade.core.Agent;
@@ -11,40 +12,67 @@ public class SightBehaviour extends TickerBehaviour {
 
     private static final long serialVersionUID = 1L;
 
-    private SuperAgent agent;
+    private Agent agent;
     private boolean isHunter;
 
     public SightBehaviour(Agent a, int period, boolean isHunter) {
         super(a, period);
 
-        agent = (SuperAgent) a;
+        agent = (Agent) a;
         this.isHunter = isHunter;
     }
 
     @Override
     protected void onTick() {
-        Maze maze = agent.getMaze();
 
-        if (maze != null) {
-            if (isHunter) {
+        if (isHunter) {
+            SuperAgent thisAgent = (SuperAgent) agent;
+
+            Maze maze = thisAgent.getMaze();
+
+            if (maze != null && !maze.isOver()) {
                 // Verify is prey is in vision
-                Maze.MazeEntity visibleEntity = maze.getVisibleEntity(agent.getName());
+                Maze.MazeEntity visibleEntity = maze.getVisibleEntity(thisAgent.getName());
                 // If yes send info to other hunters
                 if (visibleEntity != null && !visibleEntity.isHunter && !visibleEntity.isCaught()) {
 
-                    DFAgentDescription[] predators = agent.getPredators();
+                    DFAgentDescription[] predators = thisAgent.getPredators();
                     String message = "Prey " + String.valueOf(visibleEntity.getXCoordinate()) + " "
                             + String.valueOf(visibleEntity.getYCoordinate());
 
                     for (int i = 0; i < predators.length; i++) {
-                        agent.sendMessageTo(predators[i].getName(), ACLMessage.INFORM, message);
+                        thisAgent.sendMessageTo(predators[i].getName(), ACLMessage.INFORM, message);
                     }
-                    agent.setPreyX(visibleEntity.getXCoordinate());
-                    agent.setPreyY(visibleEntity.getYCoordinate());
+                    thisAgent.setPreyX(visibleEntity.getXCoordinate());
+                    thisAgent.setPreyY(visibleEntity.getYCoordinate());
 
-                    if (!(agent.getCurrentBehaviour() instanceof ChaseBehaviour)) {
-                        agent.removeCurrentBehaviour();
-                        agent.setCurrentBehaviour(agent.getChaseBehaviour());
+                    if (!(thisAgent.getCurrentBehaviour() instanceof ChaseBehaviour)) {
+                        thisAgent.removeCurrentBehaviour();
+                        thisAgent.setCurrentBehaviour(thisAgent.getChaseBehaviour());
+                    }
+                }
+            }
+        } else {
+            Prey thisAgent = (Prey) agent;
+
+            Maze maze = thisAgent.getMaze();
+            Maze.MazeEntity self = maze.getEntities().get(thisAgent.getName());
+
+            if (maze != null && !maze.isOver() && !self.isCaught()) {
+                Maze.Direction direction = self.getDirection();
+                Maze.MazeEntity visibleEntity = maze.getVisibleEntity(thisAgent.getName());
+
+                // If there is a visible hunter, mark the movement in the direction it was seen
+                // to be avoided for ticksToAvoid ticks
+                if (visibleEntity != null && visibleEntity.isHunter) {
+                    if (direction.equals(Maze.Direction.North)) {
+                        thisAgent.getMovementsToAvoid().put(Maze.Movement.Up, thisAgent.getTicksToAvoid());
+                    } else if (direction.equals(Maze.Direction.South)) {
+                        thisAgent.getMovementsToAvoid().put(Maze.Movement.Down, thisAgent.getTicksToAvoid());
+                    } else if (direction.equals(Maze.Direction.East)) {
+                        thisAgent.getMovementsToAvoid().put(Maze.Movement.Right, thisAgent.getTicksToAvoid());
+                    } else if (direction.equals(Maze.Direction.West)) {
+                        thisAgent.getMovementsToAvoid().put(Maze.Movement.Left, thisAgent.getTicksToAvoid());
                     }
                 }
             }
